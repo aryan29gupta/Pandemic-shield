b90a043bfe362aec1b68ded608e977628cf0f3c1
TaankKrish <chandrakrish2006@gmail.com>
2025-01-25 13:38:11 +0530

    added all the pages

--- backend/loginpage/urls.py
+++ backend/loginpage/urls.py
@@ -4,4 +4,8 @@ from loginpage import views
 
 urlpatterns = [
     path('', views.index,name="loginpage"),
+    path('home/', views.home,name="home"),
+    path('symptoms/', views.index,name="symptoms"),
+    path('checkarea/', views.index,name="checkarea"),
+    path('safeport/', views.index,name="safeport"),
 ]
